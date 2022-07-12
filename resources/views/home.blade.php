30c84c288b721f1c5cfac12e343aaf810323ea14
AlessandroFulco <alessandro.fulco99@gmail.com>
2022-07-12 17:09:27 +0200

    funzionalita base comics

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,36 @@
+@extends('templates.base')
 
+@section('header')
+    <header>
+        <h1>sono un HEADER</h1>
+    </header>
+@endsection
+
+@section('series')
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="container">
+                    <div class="row">
+                        @foreach ($arrComics as $comics)
+                            <div class="col-2">
+                                <div class="my-card">
+                                    <img class="img-card" src="{{$comics['thumb']}}" alt=" {{ $comics['title']}}">
+                                    <h5 class="title-comic">
+                                        {{ $comics['series']}}
+                                    </h5>
+
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
 
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/', function () {
-    return view('home');
-});
+    return view('home', [
+        'arrComics' => config('comics')
+    ]);
+})->name('home');
